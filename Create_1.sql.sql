9c10a1fd57d6486a56f6a64dc539ceab00487da9
Sergey K <katarsis1234@yandex.ru>
2022-12-07 19:40:46 +0300

    Доработка

--- Create_1.sql.sql
+++ Create_1.sql.sql
@@ -1,51 +1,52 @@
-CREATE TABLE IF NOT EXISTS Genre(
+CREATE TABLE IF NOT EXISTS genre(
      id serial PRIMARY KEY,
-     Name varchar(40) NOT NULL
+     name varchar(40) NOT NULL UNIQUE
 );
 
 
-CREATE TABLE IF NOT EXISTS List_of_performers(
+CREATE TABLE IF NOT EXISTS list_of_performers(
      id serial PRIMARY KEY,
-     Name varchar(40) NOT NULL
+     name varchar(40) NOT NULL UNIQUE 
      
 );
 
-CREATE TABLE IF NOT EXISTS Album_list(
+CREATE TABLE IF NOT EXISTS album_list(
      id serial PRIMARY KEY,
-     Name varchar(40) NOT NULL,
-     Year_of_issure integer NOT NULL     
+     name varchar(40) NOT NULL UNIQUE ,
+     year_of_issure integer check(year_of_issure>1960 AND year_of_issure<2025)     
 );
 
-CREATE TABLE IF NOT EXISTS Track_list(
+CREATE TABLE IF NOT EXISTS track_list(
      id serial PRIMARY KEY,
-     name varchar(40) NOT NULL,
-     duration integer NOT NULL
+     name varchar(40) NOT NULL UNIQUE ,
+     album_id integer REFERENCES album_list (id),
+     duration integer check(duration>0)
 );
 
 
-CREATE TABLE IF NOT exists Compilation(
-     id serial PRIMARY KEY,
-     name varchar(40) NOT NULL,
-     Year_of_issure integer NOT NULL 
+CREATE TABLE IF NOT exists compilation(
+     id serial primary key,
+     name varchar(40) NOT NULL  UNIQUE ,
+     year_of_issure integer check(year_of_issure>1960 AND year_of_issure<2025) 
 );
 
 
-CREATE TABLE IF NOT EXISTS  Track_Compilation(
-     Track_list_id integer references Track_list(id),
-     Compilation_id integer references Compilation(id),
-     constraint Tr_Co primary key (Track_list_id, Compilation_id) 
+CREATE TABLE IF NOT exists track_compilation(
+     track_list_id  integer references track_list (id),
+     compilation_id integer references compilation(id),
+     constraint tr_comp primary key (track_list_id , compilation_id) 
 );
 
 
-CREATE TABLE IF NOT EXISTS  Performers_Album(
-    Performers_id INTEGER references List_of_performers(id),
-    Album_id integer references Album_list(id),
-    constraint Pe_Al primary key (Performers_id, Album_id)
+create table if not exists performers_album(
+    album_id INTEGER references album_list (id),
+    performers_id  INTEGER  references list_of_performers(id),
+    constraint pe_al primary key (album_id, performers_id)
 );
 
 
-CREATE TABLE IF NOT EXISTS Genre_Performers(
-    Performers_id integer references List_of_performers(id),
-    Genre_id integer references Genre(id),
-    constraint Ge_Pe primary key (Performers_id, Genre_id)
+create table if not exists genre_performers(
+    performers_id  INTEGER  references list_of_performers (id),
+    genre_id INTEGER  references Genre(id),
+    constraint ge_pe primary key (performers_id , genre_id)
 );
